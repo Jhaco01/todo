437a88f613b20749ad93ddf3af3e341bf5e54ccf
Jhacoby K. Delgado <jkeithd28@gmail.com>
2022-06-03 21:28:35 -0400

    primer commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { Todo } from './js/todo.class.js';
+import './styles.css';
+
+const tarea = new Todo('Aprender JavaScript');
+console.log(tarea);
---
+++ webpack.prod.js
@@ -0,0 +1,80 @@
+const HtmlWebpack = require('html-webpack-plugin');
+const MiniCssExtract = require('mini-css-extract-plugin');
+const Copy = require("copy-webpack-plugin");
+const CssMinimizer  = require("css-minimizer-webpack-plugin");
+const Terser = require("terser-webpack-plugin");
+
+module.exports = {
+    mode: "production",
+
+    output: {
+        clean: true,
+        filename: 'name.[contenthash].js'
+    },
+
+    module: {
+        rules: [
+            {                
+                test : /\.html$/,
+                loader : 'html-loader',
+                options : {
+                    sources: false,
+                }
+            },
+            {
+                test : /\.css$/,
+                exclude: /styles.css$/,
+                use :['style-loader','css-loader']                
+            },
+            {
+                test : /styles.css$/,                
+                use: [MiniCssExtract.loader, "css-loader"],
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                  {
+                    loader: 'file-loader',
+                  },
+                ],
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: "babel-loader",
+                  options: {
+                    presets: ['@babel/preset-env']
+                  }
+                }
+            },
+        ]
+    },
+
+    optimization: {
+        minimize: true,
+        minimizer: [
+            // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+            // `...`,
+            new CssMinimizer(),
+            new Terser(),
+          ],
+    },
+
+    plugins: [
+        new HtmlWebpack({
+            title: 'Mi Webpack App',
+            template: './src/index.html'
+        }),
+        new MiniCssExtract({
+            filename: '[name].[fullhash].css',
+            ignoreOrder: false,
+        }),
+        new Copy({
+            patterns: [
+              { from: "src/assets/", to: "assets/" },
+            ],
+        }),
+    ],
+
+}
